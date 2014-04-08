fb7e47cdc451a4e146b74048dc80ee134abadf79
juliocnsouzadev <juliocnsouzadev@gmail.com>
2014-04-08 07:55:18 -0300

    generics

---
+++ src/br/com/juliocnsouza/ocpjp/generics/TestesGenerics2.java
@@ -0,0 +1,23 @@
+package br.com.juliocnsouza.ocpjp.generics;
+
+import java.io.Serializable;
+
+/**
+ * TestesGenerics2.java -> Job:
+ * <p>
+ * @since 07/04/2014
+ * @version 1.0
+ * @author Julio Cesar Nunes de Souza (julio.souza@mobilitasistemas.com.br)
+ */
+public class TestesGenerics2 {
+
+    public static void main( String[] args ) {
+        ObjetoTeste<String , Integer> test1 = new ObjetoTeste<>( "String" , 10 );
+        ObjetoTeste<? , ?> teste = new ObjetoTeste<>( 12.3f , 's' );
+        ObjetoTeste<? extends Serializable , String> teste3 = new ObjetoTeste<>(
+                1.00f ,
+                "String" );
+
+    }
+
+}
