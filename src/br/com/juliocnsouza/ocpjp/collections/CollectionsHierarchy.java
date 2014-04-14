d4fe8a47ea56045a09db252a341da966027c9ed9
juliocnsouza <juliocnsouzadev@gmail.com>
2014-04-14 15:43:27 -0300

    collections , iterator

--- src/br/com/juliocnsouza/ocpjp/collections/CollectionsHierarchy.java
+++ src/br/com/juliocnsouza/ocpjp/collections/CollectionsHierarchy.java
@@ -10,29 +10,58 @@ import java.util.LinkedHashSet;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.PriorityQueue;
 import java.util.TreeMap;
 import java.util.TreeSet;
 import java.util.WeakHashMap;
 
 /**
- * TestsWithCollections.java -> Job:
+ * CollectionsHierarchy.java -> Run this class to see the hierarchy of
+ * Collections frameword classes and interfaces:
  * <p>
  * @since 08/04/2014
  * @version 1.0
  * @author Julio Cesar Nunes de Souza (julio.souza@mobilitasistemas.com.br)
  */
-public class TestsWithCollections {
+public class CollectionsHierarchy {
 
     public static final String SEPARATOR = "\n------------------------------------------------------------------\n";
 
     public static void main( String[] args ) {
         List<Collection> collections = new ArrayList<>();
 
-        collections.add( new LinkedList<>() );
-        collections.add( new ArrayList<>() );
-        collections.add( new HashSet<>() );
-        collections.add( new LinkedHashSet<>() );
-        collections.add( new TreeSet<>() );
+        collections.add( new LinkedList<>() ); /*
+         * LinkedList implementa internamente uma estrutura duplamente linkada,
+         * mais rápida para inserir ou remover elementos, porém lenta para
+         * buscar elementos. Pode ser usada coo uma Stack (LIFO) ou comoco queue
+         * (FIFO). Permite duplicatas
+         */
+
+        collections.add( new ArrayList<>() ); /*
+         * ArrayList - implementada como um array de tamanho ajustavel. Rápida
+         * para buscar, mas lenta para inserir ou deletar. Permite duplicatas
+         */
+
+        collections.add( new HashSet<>() );/*
+         * HashSet - internamente implementada com uma hash-table. Nao permite
+         * duplicatas. Rapida para buscar e reter elementos, nao mantem uma
+         * ordem para os elementos armazenados
+         */
+
+        collections.add( new LinkedHashSet<>() );/*
+         *
+         */
+
+        collections.add( new TreeSet<>() );/*
+         * TreeSet, nao permite duplicatas, arzena elementos ordenados, utiliza
+         * uma estrutura de arvore para decidir onde armazenar ou buscar
+         * elementos
+         */
+
+        collections.add( new PriorityQueue<String>() );/*
+         * Implementada com uma estrutura de dados heap, é usada para recuperar
+         * elementos baseado em prioridade, independente da ordem de inserção.
+         */
 
         for ( Collection collection : collections ) {
             showHierarchy( collection , Collection.class );
@@ -40,8 +69,17 @@ public class TestsWithCollections {
         }
 
         List<Map<String , String>> maps = new ArrayList<>();
-        maps.add( new HashMap<String , String>() );
-        maps.add( new TreeMap<String , String>() );
+        maps.add( new HashMap<String , String>() );/*
+         * Usa hashing para encontrar um lucar para buscar ou armazenar um par
+         * chave: valor. Inserir ou adicionar é muito rápido. nao armazena os
+         * elementos em nenhuma orde
+         */
+
+        maps.add( new TreeMap<String , String>() );/*
+         * Armazena os elementos ordenados, utiliza uma estrutura de arvore para
+         * decidir onde armazenar ou buscar por chaves
+         */
+
         maps.add( new WeakHashMap<String , String>() );
         maps.add( new LinkedHashMap<String , String>() );
         maps.add( new IdentityHashMap<String , String>() );
---
+++ src/br/com/juliocnsouza/ocpjp/collections/IteratorTests.java
@@ -0,0 +1,40 @@
+package br.com.juliocnsouza.ocpjp.collections;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Iterator;
+
+/**
+ * IteratorTests.java -> Job:
+ * <p>
+ * @since 14/04/2014
+ * @version 1.0
+ * @author Julio Cesar Nunes de Souza (julio.souza@mobilitasistemas.com.br)
+ */
+public class IteratorTests {
+
+    public static void main( String[] args ) {
+        Collection<Integer> inteiros = new ArrayList<>(
+                Arrays.asList( 1 , 2 , 3 , 4 , 5 , 6 , 7 , 8 , 9 , 10 ) ); //inicializando lista
+        Iterator<Integer> iterator = inteiros.iterator(); // pegando iterator da lista
+        while ( iterator.hasNext() ) { // percorrendo com hasNext()
+            Integer n = ( ( Integer ) iterator.next() ); //pega o elemento no iterator
+            if ( n % 2 == 0 ) {
+                System.out.println( "Numero par " + n + " permanece na lista" );
+            }
+            else {
+                System.out.println( "Numero impar " + n + " sai da lista" );
+                iterator.remove(); //remove o elemento
+            }
+        }
+
+        System.out.println( "\nMostrando lista agora soh com numeros pares:" );
+        iterator = inteiros.iterator();
+        while ( iterator.hasNext() ) {
+            Integer n = ( ( Integer ) iterator.next() );
+            System.out.println( "-> " + n );
+        }
+    }
+
+}
