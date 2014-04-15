74eca714f062ddaec1d360e2fe01c81b8feae13e
juliocnsouza <juliocnsouzadev@gmail.com>
2014-04-15 12:37:28 -0300

    HashSet

---
+++ src/br/com/juliocnsouza/ocpjp/collections/HashSetTest.java
@@ -0,0 +1,29 @@
+package br.com.juliocnsouza.ocpjp.collections;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * HashSetTest.java -> Job:
+ * <p>
+ * @since 15/04/2014
+ * @version 1.0
+ * @author Julio Cesar Nunes de Souza (julio.souza@mobilitasistemas.com.br)
+ */
+public class HashSetTest {
+
+    public static void main( String[] args ) {
+        String sheldonsKnock = "Penny..., Penny..., Penny...";
+
+        StringBuilder normalKnockBuilder = new StringBuilder();
+        Set<String> words = new HashSet<>();
+        words.addAll( Arrays.asList( sheldonsKnock.split( "\\W+" ) ) );
+        for ( String string : words ) {
+            normalKnockBuilder.append( string ).append( " " );
+        }
+        System.out.println( "Sheldon's knock: " + sheldonsKnock );
+        System.out.println( "Nomal knock: " + normalKnockBuilder.toString() );
+    }
+
+}
