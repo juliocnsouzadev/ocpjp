3e7c94ac90bdfab050bce329465e0c92e1f1f6d2
juliocnsouza <juliocnsouzadev@gmail.com>
2014-12-22 21:34:45 -0200

    really simple assertion (habilitar assertions no netbeans)

--- src/br/com/juliocnsouza/ocpjp/_1assertions_exceptions/ReallySimpleAssertion.java
+++ src/br/com/juliocnsouza/ocpjp/_1assertions_exceptions/ReallySimpleAssertion.java
@@ -3,6 +3,8 @@ package br.com.juliocnsouza.ocpjp._1assertions_exceptions;
 /**
  * ReallySimpleAssertion.java -> Job:
  * <p>
+ * P
+ * <p>
  * @since 22/12/2014
  * @version 1.0
  * @author Julio Cesar Nunes de Souza (julio.souza@mobilitasistemas.com.br)
@@ -14,6 +16,11 @@ public class ReallySimpleAssertion {
         return paraDividir / 2;
     }
 
+    /*
+     * Importante! É necessário habilitar as assertions para ver o resultado! Se você estiver usando
+     * NetBeans siga os passos: 1 clique com o lado direito sobre o projeto e selecione propriedades
+     * 2 vá até o campo VM Options e digite: -ea 3 Dê um OK e execute a classe main
+     */
     public static void main( String[] args ) {
         ReallySimpleAssertion reallySimple = new ReallySimpleAssertion();
         double resultado = reallySimple.dividirPor2( 12 );
