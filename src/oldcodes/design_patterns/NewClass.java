b6a4463cd5145f9727f320d81c26d51899035efb
juliocnsouza <juliocnsouzadev@gmail.com>
2014-12-22 09:19:34 -0200

    migrating for old codes package

---
+++ src/oldcodes/design_patterns/NewClass.java
@@ -0,0 +1,13 @@
+package oldcodes.design_patterns;
+
+
+/**
+ * NewClass.java -> Job:
+ * @since 22/12/2014
+ * @version 1.0
+ * @author Julio Cesar Nunes de Souza (julio.souza@mobilitasistemas.com.br)
+ */
+
+public class NewClass {
+
+}
