b4205fcc1f3133b8c8b67e989827d611efff62a1
juliocnsouza <juliocnsouzadev@gmail.com>
2014-12-23 19:43:44 -0200

    como não usar assertivas

---
+++ src/br/com/juliocnsouza/ocpjp/_1assertions_exceptions/YouShouldNotDo.java
@@ -0,0 +1,26 @@
+package br.com.juliocnsouza.ocpjp._1assertions_exceptions;
+
+import java.util.Random;
+
+/**
+ * YouShouldNotDo.java -> Job:
+ * <p>
+ * @since 23/12/2014
+ * @version 1.0
+ * @author Julio Cesar Nunes de Souza (julio.souza@mobilitasistemas.com.br)
+ */
+public class YouShouldNotDo {
+
+    public boolean verifica( int i ) {
+        return i < 0;
+    }
+
+    //nao é recomendado usar assertions em métodos publicos
+    //e nem usar assertions de formar que possam alterar o estado do programa
+    //uma assertiva deve deixar o programa no mesmo estado que antes da expressao
+    public void a() {
+        assert ( verifica( new Random().nextInt() ) );
+        //mais código aqui
+    }
+
+}
