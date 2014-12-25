3e9b4658c03508500ada5921b54d5936acb90f60
juliocnsouza <juliocnsouzadev@gmail.com>
2014-12-25 14:35:02 -0200

    assertion tests

---
+++ src/br/com/juliocnsouza/ocpjp/_1assertions/tests/AssertTest3.java
@@ -0,0 +1,19 @@
+package br.com.juliocnsouza.ocpjp._1assertions.tests;
+
+/*
+ * A S S E R T I O N S
+ */
+public class AssertTest3 {
+
+    private static boolean isValid( int i ) {
+        return 10 % i > 0;
+    }
+
+    public static void main( String[] args ) {
+        int i = 0;
+        assert isValid( i ) : "Valor Inválido"; 
+        		// assert expressao1 : expressao2
+        System.out.println( i );
+    }
+
+}
