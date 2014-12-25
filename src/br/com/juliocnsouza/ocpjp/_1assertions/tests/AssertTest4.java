19d9f51a7a42cca7645b1295717ec8e9acf34984
juliocnsouza <juliocnsouzadev@gmail.com>
2014-12-25 16:51:41 -0200

    assertion tests

--- src/br/com/juliocnsouza/ocpjp/_1assertions/tests/AssertTest4.java
+++ src/br/com/juliocnsouza/ocpjp/_1assertions/tests/AssertTest4.java
@@ -3,7 +3,7 @@ package br.com.juliocnsouza.ocpjp._1assertions.tests;
 /*
  * A S S E R T I O N S
  */
-public class AssertTest3 {
+public class AssertTest4 {
 
     private static boolean isValid( int i ) {
         return 10 % i > 0;
@@ -12,7 +12,7 @@ public class AssertTest3 {
     public static void main( String[] args ) {
         int i = 0;
         assert isValid( i ) : "Valor Inválido"; 
-    //  assert  expressao1  :     expressao2
+        		// assert expressao1 : expressao2
         System.out.println( i );
     }
 
