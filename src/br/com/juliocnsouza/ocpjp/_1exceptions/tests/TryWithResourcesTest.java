1b9c175b65283d42f36f5a25f48df5d78ab0fb2e
juliocnsouza <juliocnsouzadev@gmail.com>
2014-12-26 15:53:08 -0200

    try-with-resources tests

---
+++ src/br/com/juliocnsouza/ocpjp/_1exceptions/tests/TryWithResourcesTest.java
@@ -0,0 +1,33 @@
+package br.com.juliocnsouza.ocpjp._1exceptions.tests;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+/**
+ * TryWithResourcesTest.java -> Job:
+ * <p>
+ * @since 26/12/2014
+ * @version 1.0
+ * @author Julio Cesar Nunes de Souza (julio.souza@mobilitasistemas.com.br)
+ */
+public class TryWithResourcesTest {
+
+    public static void main( String args[] ) {
+        char[] in = new char[ 50 ];
+        int size = 0;
+        File file = new File( "fileWriter2.txt" );
+        try ( FileWriter fw = new FileWriter( file ) ,
+                FileReader fr = new FileReader( file ) ) {
+            fw.write( "howdy\nfolks\n" );
+            size = fr.read( in );
+            System.out.print( size + " " );
+            for ( char c : in ) {
+                System.out.print( c );
+            }
+        }
+        catch ( IOException e ) {
+        }
+    }
+}
