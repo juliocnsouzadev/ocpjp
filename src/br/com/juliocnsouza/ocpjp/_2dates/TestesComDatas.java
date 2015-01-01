86d8e67ec3e66e08eb4a795653c26f913b724c58
juliocnsouza <juliocnsouzadev@gmail.com>
2014-12-31 22:03:10 -0200

    buscar dia da semana trabalhando com Calendar

--- src/br/com/juliocnsouza/ocpjp/_2dates/TestesComDatas.java
+++ src/br/com/juliocnsouza/ocpjp/_2dates/TestesComDatas.java
@@ -1,5 +1,6 @@
 package br.com.juliocnsouza.ocpjp._2dates;
 
+import java.util.Calendar;
 import java.util.Date;
 
 public class TestesComDatas {
@@ -10,5 +11,47 @@ public class TestesComDatas {
 
         data.setTime( data.getTime() + 3_600_000 ); //acrescentando 1 hora
         System.out.println( "uma hora depois... " + data.toString() );
+
+        Calendar cal = Calendar.getInstance();
+        System.out.println( "Instancia de Calendar é da classe concreta: " + cal.getClass() );
+
+        cal.setTime( data ); //setando a data para o nosso objeto calendar
+
+        boolean iniciaDomingo = Calendar.SUNDAY == cal.getFirstDayOfWeek();//aqui verificamos se o primeiro dia setado na jvm é domingo (em alguns locais pode ser na segunda feira)
+
+        int diaSemana = cal.get( Calendar.DAY_OF_WEEK ); //retorna um inteiro representando o dia da semana
+        String diaSemanaStr = getDiaSemana( iniciaDomingo , diaSemana );
+        System.out.println(
+                "Um trilhão de milisegundos caiu no seguinte dia da semana: " + diaSemanaStr );
+
+    }
+
+    public static String getDiaSemana( boolean iniciaDomingo , int diaSemana ) {
+        if ( !iniciaDomingo ) {
+            if ( diaSemana == 7 ) {
+                diaSemana = 1;
+            }
+            else {
+                diaSemana++;
+            }
+        }
+        switch ( diaSemana ) {
+            case 1:
+                return "Domingo";
+            case 2:
+                return "Segunda Feira";
+            case 3:
+                return "Terça Feira";
+            case 4:
+                return "Quarta Feira";
+            case 5:
+                return "Quinta Feira";
+            case 6:
+                return "Sexta Feira";
+            case 7:
+                return "Sábado";
+            default:
+                return diaSemana + " Não é um dia da semana válido";
+        }
     }
 }
