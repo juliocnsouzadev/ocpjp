3e73be4d3a7aba64d659414701da7c1e2f7d1de2
juliocnsouza <juliocnsouzadev@gmail.com>
2014-12-31 23:24:42 -0200

    DateFormat parse()

--- src/br/com/juliocnsouza/ocpjp/_2dates/TestesComDateFormat.java
+++ src/br/com/juliocnsouza/ocpjp/_2dates/TestesComDateFormat.java
@@ -1,6 +1,7 @@
 package br.com.juliocnsouza.ocpjp._2dates;
 
 import java.text.DateFormat;
+import java.text.ParseException;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
@@ -19,6 +20,20 @@ public class TestesComDateFormat {
 
         for ( DateFormat df : dfs ) {
             System.out.println( df.format( data ) );
+        }
+
+        String dataValida = "10/10/2012";
+        String dataInvalida = "dez de outubro de 2012";
+
+        try {
+            DateFormat df = DateFormat.getDateInstance();
+            Date data1 = df.parse( dataValida );
+            System.out.println( "Data válida = " + data1.toString() );
+            Date data2 = df.parse( dataInvalida );
+            System.out.println( "Data inválida = " + data2.toString() );
+        }
+        catch ( ParseException pe ) {
+            System.out.println( "ParseException: " + pe.getMessage() );
         }
 
     }
