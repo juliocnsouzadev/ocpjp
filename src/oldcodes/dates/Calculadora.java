ab934a6ea9acbbc565c3d0f7a02ac3833afc1756
juliocnsouza <juliocnsouzadev@gmail.com>
2015-01-09 17:24:28 -0200

    Calc datas

---
+++ src/oldcodes/dates/Calculadora.java
@@ -0,0 +1,68 @@
+package oldcodes.dates;
+
+import java.util.Calendar;
+import java.util.Date;
+
+/**
+ * Calculadora.java -> Job:
+ * <p>
+ * @since 09/01/2015
+ * @version 1.0
+ * @author Julio Cesar Nunes de Souza (julio.souza@mobilitasistemas.com.br)
+ */
+public class Calculadora {
+
+    public static long getDias( Date dataInicial , Date dataFinal ) {
+
+        if ( dataFinal == null || dataInicial == null ) {
+            return 0;
+        }
+
+        long timeF = dataFinal.getTime();
+        long timeI = dataInicial.getTime();
+
+        if ( timeF < timeI ) {
+            return 0;
+        }
+
+        Date dataDif = new Date( timeF + timeI );
+
+        long dia = 1000 * 60 * 60 * 24;
+
+        long diasDif = dataDif.getTime() / dia;
+
+        Calendar inicialD = Calendar.getInstance();
+        Calendar finalD = Calendar.getInstance();
+
+        inicialD.setTime( dataInicial );
+        finalD.setTime( dataFinal );
+
+        int anoI = inicialD.get( Calendar.YEAR );
+        int anoF = finalD.get( Calendar.YEAR );
+
+        int bissextos = getBissextos( anoI , anoF );
+
+        diasDif += bissextos;
+
+        return diasDif;
+    }
+
+    private static int getBissextos( int anoI , int anoF ) {
+        int counter = 0;
+        for ( int i = anoI ; i <= anoF ; i++ ) {
+            if ( i % 4 == 0 ) {
+                counter++;
+            }
+        }
+        return counter;
+    }
+
+    public static void main( String[] args ) {
+        Date dI = new Date( 2000000l );
+        Date dF = new Date();
+
+        System.out.println( "-> " + getDias( dI , dF ) );
+
+    }
+
+}
