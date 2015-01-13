ecfb1550f9f2d1cb307fcbce9ecaafcc1e495ce7
juliocnsouza <juliocnsouzadev@gmail.com>
2015-01-13 06:11:55 -0200

    ajustes

--- src/oldcodes/dates/Calculadora.java
+++ src/oldcodes/dates/Calculadora.java
@@ -2,7 +2,6 @@ package oldcodes.dates;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.Calendar;
 import java.util.Date;
 
 public class Calculadora {
@@ -25,31 +24,7 @@ public class Calculadora {
         Date dataDif = new Date( timeF - timeI );
         long diasDif = ( dataDif.getTime() / DIA_EM_MILIS );
 
-        //sem calcular bissextos, já que o milis deve prove-los
-        //int bissextos = calcularAnosBissextos( dataInicial , dataFinal );
-        //diasDif += bissextos;
         return diasDif + 1; //esse + um é para se contar o dia atual
-    }
-
-    private static int calcularAnosBissextos( Date dataInicial , Date dataFinal ) {
-        Calendar inicialD = Calendar.getInstance();
-        Calendar finalD = Calendar.getInstance();
-        inicialD.setTime( dataInicial );
-        finalD.setTime( dataFinal );
-        int anoI = inicialD.get( Calendar.YEAR );
-        int anoF = finalD.get( Calendar.YEAR );
-        int bissextos = getBissextos( anoI , anoF );
-        return bissextos;
-    }
-
-    private static int getBissextos( int anoI , int anoF ) {
-        int counter = 0;
-        for ( int i = anoI ; i <= anoF ; i++ ) {
-            if ( i % 4 == 0 ) {
-                counter++;
-            }
-        }
-        return counter;
     }
 
     public static void main( String[] args ) {
