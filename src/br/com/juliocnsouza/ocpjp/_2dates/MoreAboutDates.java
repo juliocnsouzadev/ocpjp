c956482f81892a36f8e4f4dea918d368bb312738
juliocnsouza <juliocnsouzadev@gmail.com>
2015-02-08 11:40:41 -0200

    formatandoDatas

--- src/br/com/juliocnsouza/ocpjp/_2dates/MoreAboutDates.java
+++ src/br/com/juliocnsouza/ocpjp/_2dates/MoreAboutDates.java
@@ -1,6 +1,10 @@
 package br.com.juliocnsouza.ocpjp._2dates;
 
+import java.text.DateFormat;
+import java.util.Arrays;
 import java.util.Calendar;
+import java.util.List;
+import java.util.Locale;
 
 /**
  * MoreAboutDates.java -> Job:
@@ -14,6 +18,37 @@ public class MoreAboutDates {
     public static void main( String[] args ) {
         iniciandoCalendar();
         addVersusRoll();
+        formatandoDatas();
+    }
+
+    private static void formatandoDatas() {
+        List<DateFormat> dfs = Arrays.asList( DateFormat.getInstance() ,
+                                              DateFormat.getDateInstance() ,
+                                              DateFormat.getDateInstance( DateFormat.SHORT ) ,
+                                              DateFormat.getDateInstance( DateFormat.MEDIUM ) ,
+                                              DateFormat.getDateInstance( DateFormat.LONG ) ,
+                                              DateFormat.getDateInstance( DateFormat.FULL ) ,
+                                              DateFormat.getDateInstance( DateFormat.SHORT ,
+                                                                          Locale.ITALIAN ) ,
+                                              DateFormat.getDateInstance( DateFormat.MEDIUM ,
+                                                                          Locale.ITALIAN ) ,
+                                              DateFormat.getDateInstance( DateFormat.LONG ,
+                                                                          Locale.ITALIAN ) ,
+                                              DateFormat.getDateInstance( DateFormat.FULL ,
+                                                                          Locale.ITALIAN ) ,
+                                              DateFormat.getDateInstance( DateFormat.SHORT ,
+                                                                          Locale.GERMAN ) ,
+                                              DateFormat.getDateInstance( DateFormat.MEDIUM ,
+                                                                          Locale.GERMAN ) ,
+                                              DateFormat.getDateInstance( DateFormat.LONG ,
+                                                                          Locale.GERMAN ) ,
+                                              DateFormat.getDateInstance( DateFormat.FULL ,
+                                                                          Locale.GERMAN ) );
+        Calendar cal = Calendar.getInstance();
+        for ( DateFormat df : dfs ) {
+            System.out.println( "Data formatada: " + df.format( cal.getTime() ) );
+            System.out.println( "" );
+        }
     }
 
     private static void addVersusRoll() {
@@ -21,7 +56,7 @@ public class MoreAboutDates {
         System.out.println( "Data: " + cal.getTime() );//saida do dia: Data: Sun Feb 08 11:19:08 BRST 2015
         cal.add( Calendar.MONTH , 13 ); //adiciona um ano e 1 mês
         System.out.println( "Data + 13 meses: " + cal.getTime() );//saida do dia: Data + 13 meses: Tue Mar 08 11:21:29 BRT 2016
-        cal.roll( Calendar.MONTH , 13 ); //adiciona 13 mesess, mas nao muda o ano
+        cal.roll( Calendar.MONTH , 13 ); //adiciona 13 meses, mas nao muda o ano
         System.out.println( "Data + 13 meses com roll: " + cal.getTime() );//saida do dia: Data + 13 meses com roll: Fri Apr 08 11:21:29 BRT 2016
 
         cal.roll( Calendar.DAY_OF_MONTH , 35 ); //nao muda o mes e ano
