5afbfb4052ebbe95e453a01613d578971d17fd1d
juliocnsouza <juliocnsouzadev@gmail.com>
2015-02-23 19:42:15 -0300

    questao 1

--- src/br/com/juliocnsouza/ocpjp/tests/StringProcessing.java
+++ src/br/com/juliocnsouza/ocpjp/tests/StringProcessing.java
@@ -1,13 +1,28 @@
 package br.com.juliocnsouza.ocpjp.tests;
 
-
 /**
  * StringProcessing.java -> Job:
+ * <p>
  * @since 23/02/2015
  * @version 1.0
  * @author Julio Cesar Nunes de Souza (julio.souza@mobilitasistemas.com.br)
  */
-
 public class StringProcessing {
+
+    static void q1() {
+        int i = 200;
+        double d = 100.00;
+        try {
+            System.console().format( "%d * %1.1f" , i , d );
+        }
+        catch ( NullPointerException npe ) {
+            System.out.println( "Não há console pela IDE" );
+        }
+
+    }
+
+    public static void main( String[] args ) {
+        q1();
+    }
 
 }
